5555bc9b655fb8adf5b1bea206a3e6239f47e3a4
joigno <30682875+joigno@users.noreply.github.com>
2023-04-24 21:53:03 +0200

    basic set-burnchain-parameters tests

---
+++ tests/set-burnchain-parameters_test.ts
@@ -0,0 +1,74 @@
+import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
+import {
+  Account,
+  Chain,
+  Clarinet,
+  Tx,
+  types,
+} from "https://deno.land/x/clarinet@v1.5.4/index.ts";
+
+Clarinet.test({
+  name: "set-burnchain-parameters: can be called successfully for the first time",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+
+    const block = chain.mineBlock([
+      Tx.contractCall(
+        "pox-2",
+        "set-burnchain-parameters",
+        [
+          types.uint(100),
+          types.uint(50),
+          types.uint(200),
+          types.uint(25),
+          types.uint(500),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    assertEquals(block.receipts.length, 1);
+    block.receipts[0].result.expectOk().expectBool(true);
+  },
+});
+
+Clarinet.test({
+  name: "set-burnchain-parameters: cannot be called twice",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+
+    chain.mineBlock([
+      Tx.contractCall(
+        "pox-2",
+        "set-burnchain-parameters",
+        [
+          types.uint(100),
+          types.uint(50),
+          types.uint(200),
+          types.uint(25),
+          types.uint(500),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    const block = chain.mineBlock([
+      Tx.contractCall(
+        "pox-2",
+        "set-burnchain-parameters",
+        [
+          types.uint(100),
+          types.uint(50),
+          types.uint(200),
+          types.uint(25),
+          types.uint(500),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    assertEquals(block.receipts.length, 1);
+    let ERR_NOT_ALLOWED = 19;
+    block.receipts[0].result.expectErr().expectInt(ERR_NOT_ALLOWED);
+  },
+});
