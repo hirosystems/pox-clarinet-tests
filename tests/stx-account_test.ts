d369a58cfcd87c185cf38b6e0e86aa65503a1c5d
Jeff Bencin <jeff.bencin@gmail.com>
2023-05-10 13:45:53 -0400

    refactor: Combine two files into `stx-account_tests.ts`

--- tests/stx-account_test.ts
+++ tests/stx-account_test.ts
@@ -9,7 +9,65 @@ import {
 import { Pox3 } from "./models/pox-3.ts";
 
 Clarinet.test({
-  name: "stx-account-from-pox-data: Check account after stacking",
+  name: "stx-account: Test stx-locked-from-pox-data",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const sender = accounts.get("wallet_1")!;
+    const pox3 = new Pox3(chain, accounts.get("deployer")!);
+    const initialAmount = 50000;
+    const startBurnHeight = 10;
+    const lockPeriod = 10;
+
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "pox-3",
+        "stack-stx",
+        [
+          types.uint(initialAmount),
+          types.tuple({
+            version: types.buff(Uint8Array.from([4])),
+            hashbytes: types.buff(
+              Uint8Array.from([
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+              ])
+            ),
+          }),
+          types.uint(startBurnHeight),
+          types.uint(lockPeriod),
+        ],
+        sender.address
+      ),
+    ]);
+
+    assertEquals(block.receipts.length, 1);
+    block.receipts[0].result.expectOk();
+
+    // Check we are on cycle 0
+    pox3.currentPoxRewardCycle()
+      .result
+      .expectUint(0);
+
+    // Confirm STX is not locked yet
+    pox3.stxLockedFromPoxData(sender.address)
+      .result
+      .expectUint(0);
+
+    // Advance to next cycle
+    chain.mineEmptyBlockUntil(3000);
+
+    // Check we are on cycle 1
+    pox3.currentPoxRewardCycle()
+      .result
+      .expectUint(1);
+
+    // Confirm STX is now locked
+    pox3.stxLockedFromPoxData(sender.address)
+      .result
+      .expectUint(initialAmount);
+  },
+});
+
+Clarinet.test({
+  name: "stx-account: Test stx-account-from-pox-data",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     const sender = accounts.get("wallet_1")!;
     const pox3 = new Pox3(chain, accounts.get("deployer")!);
