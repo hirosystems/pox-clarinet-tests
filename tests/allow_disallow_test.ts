117995c879983dad22e355566f9a7b5787cd9866
Jeff Bencin <jeff.bencin@gmail.com>
2023-05-18 16:27:34 -0400

    Check expiration of `allow-contract-caller`

--- tests/allow_disallow_test.ts
+++ tests/allow_disallow_test.ts
@@ -8,13 +8,14 @@ import {
   types,
 } from "https://deno.land/x/clarinet@v1.5.4/index.ts";
 Clarinet.test({
-  name: "Test allowing and disallowing contract callers with intermediary contract",
+  name: "allow-contract-caller: Test allowing an intermediary contract caller for a fixed # of blocks",
   async fn(chain: Chain, accounts: Map<string, Account>, contracts: Map<string, Contract>) {
     const deployer = accounts.get("deployer")!;
     const contractCaller = accounts.get("wallet_1")!;
     const user = accounts.get("wallet_2")!;
     const intermediary = contracts.get(`${deployer.address}.intermediary`)!;
     const pox = contracts.get(`${deployer.address}.pox-3`)!;
+    const allowanceExpiration = 100;
     
     // Test 1: Check that the contract caller is allowed when tx-sender == contract-caller
     chain.callReadOnlyFn('pox-3', 'check-caller-allowed', [], deployer.address)
@@ -33,7 +34,7 @@ Clarinet.test({
         'allow-contract-caller',
         [
           types.principal(intermediary.contract_id),
-          types.some(types.uint(5))
+          types.some(types.uint(allowanceExpiration))
         ],
         deployer.address
       ),
@@ -44,93 +45,24 @@ Clarinet.test({
     chain.callReadOnlyFn('intermediary', 'check-caller-allowed-proxy', [], deployer.address)
       .result
       .expectOk();
-
-    return
   
-    // Check the allowance
-    const allowance = chain.callReadOnlyFn(
-      "pox-3",
-      "get-allowance-contract-callers",
-      [types.principal(deployer.address), types.principal(contractCaller.address)],
+    // Check the allowance directly
+    chain.callReadOnlyFn(
+      'pox-3',
+      'get-allowance-contract-callers',
+      [
+        types.principal(deployer.address),
+        types.principal(intermediary.contract_id),
+      ],
       deployer.address
-    );
-    allowance.result.expectSome().expectTuple().untilBurnHt.expectUint(200);
+    ).result.expectSome().expectTuple()['until-burn-ht'].expectSome().expectUint(allowanceExpiration);
 
-    // Test 3: Check that the contract caller is now allowed
-    let allowed = chain.callReadOnlyFn(
-      "pox-3",
-      "call-check-caller-allowed",
-      [types.principal(pox.address)],
-      contractCaller.address
-    );
-    assertEquals(allowed.result.expectBool(), true);
+    // Mine until allowance expires
+    chain.mineEmptyBlock(allowanceExpiration + 1);
 
-    // Test 4: Check that the contract caller is not allowed when until-burn-ht is expired
-    chain.mineEmptyBlock(201); // Expire the until-burn-ht
-    allowed = chain.callReadOnlyFn(
-      "intermediary",
-      "call-check-caller-allowed",
-      [types.principal(pox.address)],
-      contractCaller.address
-    );
-    assertEquals(allowed.result.expectBool(), false);
-
-    // Test 5: Disallow contract caller and check that the allowance is removed
-    const disallowBlock = chain.mineBlock([
-      Tx.contractCall(
-        "pox-3",
-        "disallow-contract-caller",
-        [types.principal(contractCaller.address)],
-        deployer.address
-      ),
-    ]);
-    disallowBlock.receipts[0].result.expectOk();
-
-    // Check the allowance
-    const removedAllowance = chain.callReadOnlyFn(
-      "pox-3",
-      "get-allowance-contract-callers",
-      [types.principal(deployer.address), types.principal(contractCaller.address)],
-      deployer.address
-    );
-    removedAllowance.result.expectNone();
-
-    // Test 6: Check that the contract caller is now disallowed
-    allowed = chain.callReadOnlyFn(
-      "intermediary",
-      "call-check-caller-allowed",
-      [types.principal(pox.address)],
-      contractCaller.address
-    );
-    assertEquals(allowed.result.expectBool(), false);
-
-    // Test 7: Allow contract caller with no expiry
-    const allowNoExpiryBlock = chain.mineBlock([
-      Tx.contractCall(
-        "pox-3",
-        "allow-contract-caller",
-        [types.principal(contractCaller.address), types.none()],
-        deployer.address
-      ),
-    ]);
-    allowNoExpiryBlock.receipts[0].result.expectOk();
-
-    // Check the allowance
-    const noExpiryAllowance = chain.callReadOnlyFn(
-      "pox-3",
-      "get-allowance-contract-callers",
-      [types.principal(deployer.address), types.principal(contractCaller.address)],
-      deployer.address
-    );
-    noExpiryAllowance.result.expectSome().expectTuple().untilBurnHt.expectNone();
-
-    // Test 8: Check that the contract caller is allowed with no expiry
-    allowed = chain.callReadOnlyFn(
-      "intermediary",
-      "call-check-caller-allowed",
-      [types.principal(pox.address)],
-      contractCaller.address
-    );
-    assertEquals(allowed.result.expectBool(), true);
+    // Check that allowance has expired
+    chain.callReadOnlyFn('intermediary', 'check-caller-allowed-proxy', [], deployer.address)
+      .result
+      .expectErr();
   },
 });
